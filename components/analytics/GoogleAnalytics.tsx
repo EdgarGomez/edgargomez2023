73dea3c41d097edf5297687ac7df0ce221918ee3
Edgar J Gomez <edgar@ecomm.pro>
2023-01-14 23:59:07 +0100

    update analytics

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -9,15 +9,13 @@ const GAScript = () => {
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
       />
-
       <Script strategy="lazyOnload" id="ga-script">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
-              page_path: window.location.pathname,
-            });
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${siteMetadata.analytics.googleAnalyticsId}');
         `}
       </Script>
     </>
